d11a9e022e25de976bb6ea8f7a210f86a521feaa
Permana <sdharma721@gmail.com>
2026-03-05 21:10:12 +0700

    refactor(GameDigital): added game size in GB, implemented getSizeGB method, and updated tampilGame method

--- src/_5_Steam/GameDigital.java
+++ src/_5_Steam/GameDigital.java
@@ -1,4 +1,21 @@
 package _5_Steam;
 
 public class GameDigital extends Game{
+    private int sizeGB;
+
+    GameDigital(int idGame, String namaGame, int hargaGame, int sizeGB){
+        super(idGame, namaGame, hargaGame);
+        this.sizeGB = sizeGB;
+    }
+
+    public int getSizeGB(){
+        return sizeGB;
+    }
+
+    public void tampilGame(){
+        System.out.println("ID: " + getIdGame());
+        System.out.println("Nama: " + getNamaGame());
+        System.out.println("Harga: " + getHargaGame());
+        System.out.println("Size: " + getSizeGB());
+    }
 }
