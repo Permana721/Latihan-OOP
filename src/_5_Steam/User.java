d2f75d3e5e5f8be8d97a4e5757463af6dc24bbc3
Permana <sdharma721@gmail.com>
2026-03-05 21:10:20 +0700

    `feat(User): Add nama, saldo, game collection, purchase method`

--- src/_5_Steam/User.java
+++ src/_5_Steam/User.java
@@ -1,5 +1,30 @@
 package _5_Steam;
 
 public class User {
+    private String namaUser;
+    private int saldo;
+    private Game[] koleksi = new Game[10];
+    private int jumlahGame = 0;
 
+    public User(String namaUser, int saldo){
+        this.namaUser = namaUser;
+        this.saldo = saldo;
+    }
+
+    public void tambahSaldo(int saldo){
+        this.saldo += saldo;
+    }
+
+    public void beliGame(Game game){
+        if(saldo >= game.getHargaGame()){
+            saldo -= game.getHargaGame();
+            koleksi[jumlahGame] = game;
+            jumlahGame++;
+
+            System.out.println("Berhasil membeli " + game.getNamaGame());
+        }
+        else{
+            System.out.println("Saldo tidak cukup");
+        }
+    }
 }
