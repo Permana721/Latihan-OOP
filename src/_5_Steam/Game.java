97be759b84d72ea71c46abbff6c8aaf6cbd7991f
Permana <sdharma721@gmail.com>
2026-03-05 21:10:08 +0700

    feat(game): added game id, name and price properties, constructor, and tampilGame method

--- src/_5_Steam/Game.java
+++ src/_5_Steam/Game.java
@@ -1,4 +1,35 @@
 package _5_Steam;
 
 public class Game {
+    private int idGame;
+    private String namaGame;
+    private int hargaGame;
+
+    Game(){
+
+    }
+
+    Game(int idGame, String namaGame, int hargaGame){
+        this.idGame = idGame;
+        this.namaGame = namaGame;
+        this.hargaGame = hargaGame;
+    }
+
+    public int getIdGame(){
+        return idGame;
+    }
+
+    public String getNamaGame(){
+        return namaGame;
+    }
+
+    public int getHargaGame(){
+        return hargaGame;
+    }
+
+    public void tampilGame(){
+        System.out.println("ID: " + getIdGame());
+        System.out.println("Nama: " + getNamaGame());
+        System.out.println("Harga: " + getHargaGame());
+    }
 }
