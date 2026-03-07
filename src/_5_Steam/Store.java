1b4174c686d4397e4b61a8a91b0cbe8158ee4cea
Permana <sdharma721@gmail.com>
2026-03-07 13:27:29 +0700

    refactor(store): unify game list into single array and remove redundant methods

--- src/_5_Steam/Store.java
+++ src/_5_Steam/Store.java
@@ -3,33 +3,19 @@ package _5_Steam;
 import java.util.ArrayList;
 
 public class Store {
-    private ArrayList<GameFisik> daftarGameFisik = new ArrayList<>();
-    private ArrayList<GameDigital> daftarGameDigital = new ArrayList<>();
+    ArrayList<Game> semuaGame = new ArrayList<>();
 
-    public void tambahGame(){
-        ArrayList<GameFisik> daftarGameFisik = new ArrayList<>();
-        daftarGameFisik.add(new GameFisik(1, "Elden Ring", 500_000));
-        daftarGameFisik.add(new GameFisik(2, "Cyberpunk", 450_000));
+    public ArrayList<Game> tambahGame() {
+        semuaGame.add(new GameFisik(1, "Elden Ring", 500_000));
+        semuaGame.add(new GameFisik(2, "Cyberpunk", 650_000));
+        semuaGame.add(new GameDigital(3, "Resident Evil 9", 850_000, 120));
+        semuaGame.add(new GameDigital(4, "GTA 6", 1_500_000, 200));
 
-        ArrayList<GameDigital> daftarGameDigital = new ArrayList<>();
-        daftarGameDigital.add(new GameDigital(1, "Resident Evil 9", 850_000, 120));
-        daftarGameDigital.add(new GameDigital(2, "GTA 6", 1_500_000, 200));
+        return semuaGame;
     }
 
-    public int hitungTotalGameFisik() {
-        return daftarGameFisik.size();
-    }
-
-    public int hitungTotalGameDigital() {
-        return daftarGameDigital.size();
-    }
-
-    public ArrayList<GameFisik> getListFisik() {
-        return this.daftarGameFisik;
-    }
-
-    public ArrayList<GameDigital> getListDigital() {
-        return this.daftarGameDigital;
+    public int hitungTotalGame() {
+        return semuaGame.size();
     }
 
 }
