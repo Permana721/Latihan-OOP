ee0c5f963f9aef6d2e28131fedf59208894dc4b0
Permana <sdharma721@gmail.com>
2026-03-07 11:00:13 +0700

    feat(GameFisik): extends Game, adds constructor and tampilGameFisik method

--- src/_5_Steam/GameFisik.java
+++ src/_5_Steam/GameFisik.java
@@ -1,4 +1,17 @@
 package _5_Steam;
 
-public class GameFisik {
+public class GameFisik extends Game{
+    GameFisik(){
+
+    }
+
+    GameFisik(int idGame, String namaGame, int hargaGame){
+        super(idGame, namaGame, hargaGame);
+    }
+
+    public void tampilGameFisik(){
+        System.out.println("ID: " + getIdGame());
+        System.out.println("Nama: " + getNamaGame());
+        System.out.println("Harga: " + getHargaGame());
+    }
 }
