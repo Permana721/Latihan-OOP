99934bcb34b6b7c58d185d3149734029ab38dff9
Permana <sdharma721@gmail.com>
2026-03-07 11:00:19 +0700

    refactor(store): consolidate game listing logic for physical and digital games

--- src/_5_Steam/Main.java
+++ src/_5_Steam/Main.java
@@ -5,15 +5,10 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         boolean jalan = true;
-          Scanner input = new Scanner(System.in);
-          ArrayList<Game> daftarGame = new ArrayList<>();
-          daftarGame.add(new Game(1, "Elden Ring", 500_000));
-          daftarGame.add(new Game(2, "Cyberpunk", 450_000));
-
-        ArrayList<GameDigital> daftarGameDigital = new ArrayList<>();
-        daftarGameDigital.add(new GameDigital(1, "Resident Evil 9", 850_000, 120));
-        daftarGameDigital.add(new GameDigital(2, "GTA 6", 1_500_000, 200));
-
+        Store store = new Store();
+        GameFisik daftarGameFisik = new GameFisik();
+        GameDigital daftarGameDigital = new GameDigital();
+        Scanner input = new Scanner(System.in);
         User user = new User("Permana", 2_000_000);
         user.tambahSaldo(10_000);
 
@@ -27,20 +22,24 @@ public class Main {
 
             switch (pilihan){
                 case 1 -> {
-                    for(int i = 0; i < daftarGame.size(); i++){
-                        daftarGame.get(i).tampilGame();
+                    System.out.println("--- DAFTAR GAME FISIK ---");
+                    for (GameFisik gf : store.getListFisik()) {
+                        gf.tampilGameFisik();
+                        System.out.println("-------------------");
                     }
-                    for(int i = 0; i < daftarGameDigital.size(); i++){
-                        daftarGameDigital.get(i).tampilGame();
+                    System.out.println("\n--- DAFTAR GAME DIGITAL ---");
+                    for (GameDigital gd : store.getListDigital()) {
+                        gd.tampilGame();
+                        System.out.println("-------------------");
                     }
                 } case 2 -> {
-                    System.out.println("Masukan ID game yang ingin anda beli");
-                    int id = input.nextInt();
-                    user.beliGame(daftarGame, id);
+//                    System.out.println("Masukan ID game yang ingin anda beli");
+//                    int id = input.nextInt();
+//                    user.beliGame(daftarGameFisik, id);
                 } case 3 -> {
-                    System.out.println("Masukan ID game yang ingin anda beli");
-                    int id = input.nextInt();
-                    user.beliGameDigital(daftarGameDigital, id);
+//                    System.out.println("Masukan ID game yang ingin anda beli");
+//                    int id = input.nextInt();
+//                    user.beliGame(daftarGameDigital, id);
                 } case 4 -> {
                     user.getKoleksi();
                 } case 5 -> {
