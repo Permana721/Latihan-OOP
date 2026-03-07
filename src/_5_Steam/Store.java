98f9b0250d9a6b57c87ea81f08b1c93548d63513
Permana <sdharma721@gmail.com>
2026-03-07 11:00:27 +0700

    refactor(store): introduce array lists for game types and add initialization method

--- src/_5_Steam/Store.java
+++ src/_5_Steam/Store.java
@@ -1,4 +1,35 @@
 package _5_Steam;
 
+import java.util.ArrayList;
+
 public class Store {
+    private ArrayList<GameFisik> daftarGameFisik = new ArrayList<>();
+    private ArrayList<GameDigital> daftarGameDigital = new ArrayList<>();
+
+    public void tambahGame(){
+        ArrayList<GameFisik> daftarGameFisik = new ArrayList<>();
+        daftarGameFisik.add(new GameFisik(1, "Elden Ring", 500_000));
+        daftarGameFisik.add(new GameFisik(2, "Cyberpunk", 450_000));
+
+        ArrayList<GameDigital> daftarGameDigital = new ArrayList<>();
+        daftarGameDigital.add(new GameDigital(1, "Resident Evil 9", 850_000, 120));
+        daftarGameDigital.add(new GameDigital(2, "GTA 6", 1_500_000, 200));
+    }
+
+    public int hitungTotalGameFisik() {
+        return daftarGameFisik.size();
+    }
+
+    public int hitungTotalGameDigital() {
+        return daftarGameDigital.size();
+    }
+
+    public ArrayList<GameFisik> getListFisik() {
+        return this.daftarGameFisik;
+    }
+
+    public ArrayList<GameDigital> getListDigital() {
+        return this.daftarGameDigital;
+    }
+
 }
